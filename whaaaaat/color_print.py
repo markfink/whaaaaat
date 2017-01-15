95ef78fcca685feb2e4adcce1023c5dc6ab34a43
mark <mark@mark-fink.de>
2017-01-15 18:05:30 +0100

    simple colorized output

---
+++ whaaaaat/color_print.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+provide colorized output
+"""
+from __future__ import print_function, unicode_literals
+
+from prompt_toolkit.shortcuts import print_tokens, style_from_dict, Token
+
+
+def _print_token_factory(col):
+    """Internal helper to provide color names."""
+    def _helper(msg):
+        style = style_from_dict({
+            Token.Color: col,
+        })
+        tokens = [
+            (Token.Color, msg)
+        ]
+        print_tokens(tokens, style=style)
+    return _helper
+
+# used this for color source:
+# http://unix.stackexchange.com/questions/105568/how-can-i-list-the-available-color-names
+yellow = _print_token_factory('#dfaf00')
+blue = _print_token_factory('#0087ff')
+gray = _print_token_factory('#6c6c6c')
+
+# TODO
+#black
+#red
+#green
+#magenta
+#cyan
+#white
