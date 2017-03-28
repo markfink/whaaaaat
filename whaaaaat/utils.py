dac5c0ca40b26788b42d9b584a65ea93673b3589
Lele Gaifax <lele@metapensiero.it>
2017-03-28 12:16:33 +0200

    Add Python 3 compatibility

--- whaaaaat/utils.py
+++ whaaaaat/utils.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import json
+import sys
 
 from pygments import highlight, lexers, formatters
 
 __version__ = '0.1.2'
+
+PY3 = sys.version_info[0] >= 3
 
 
 def format_json(data):
@@ -12,7 +15,13 @@ def format_json(data):
 
 
 def colorize_json(data):
-    colorful_json = highlight(unicode(data, 'UTF-8'),
+    if PY3:
+        if isinstance(data, bytes):
+            data = data.decode('UTF-8')
+    else:
+        if not isinstance(data, unicode):
+            data = unicode(data, 'UTF-8')
+    colorful_json = highlight(data,
                               lexers.JsonLexer(),
                               formatters.TerminalFormatter())
     return colorful_json
